09b061a2ceb190ef66a262b7854d11499dfdbc46
soo.yongjie <soo.yongjie@growthops.asia>
2022-06-12 16:21:38 +0800

    Updated media with mobile optimisation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import twitter from './twitter.svg';
-import discord from './discord.svg';
-import opensea from './opensea.svg';
+import twitter from './img/twitter.png';
+import opensea from './img/opensea.png';
+import etherscan from './img/etherscan.png';
+import connectWallet from './img/connect_wallet.png';
+import logo from './img/logo.png';
+import video from './img/video.webm';
+
 import sample from './sample1.jpg';
 
 function App() {
   return (
     <div className="container">
+      <div className="logo-wrapper">
+        <img src={logo} alt="" className='logo' />
+      </div>
       <div className="wrapper">
         <div className="showcase">
-          <img src={sample} alt="" />
+          {/* <img src={sample} alt="" /> */}
+          <video src={video} autoPlay loop muted></video>
         </div>
         <div className="mint-box">
           <div className="links">
             <a href="">
-              <img src={discord} alt="" />
+              <img src={twitter} alt="" />
             </a>
             <a href="">
               <img src={opensea} alt="" />
             </a>
             <a href="">
-              <img src={twitter} alt="" />
+              <img src={etherscan} alt="" />
             </a>
           </div>
           <div className="desc">
@@ -28,7 +36,9 @@ function App() {
             </p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem asperiores dolorum totam.</p>
           </div>
-          <button>Connect Wallet</button>
+          <button>
+            <img src={connectWallet} alt="" />
+          </button>
         </div>
       </div>
       <div className="banner">
